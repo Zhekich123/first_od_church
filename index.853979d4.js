3edef56faa44062db08da0b9b497d30dd1631aac
Zhekich123 <axelb380@gmail.com>
2023-07-15 23:30:44 +0300

    Updates

---
+++ index.853979d4.js
@@ -0,0 +1,2 @@
+"use strict";const e=document.querySelector(".service-item__link"),t=document.querySelector(".service-item");e.addEventListener("click",function(e){e.preventDefault(),t.classList.toggle("open")});
+//# sourceMappingURL=index.853979d4.js.map
