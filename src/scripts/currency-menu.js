568b7348c16b4aff0f47861a3adfec83f13cba95
Zhekich123 <axelb380@gmail.com>
2023-08-07 21:53:17 +0300

    making requisites

--- src/scripts/currency-menu.js
+++ src/scripts/currency-menu.js
@@ -45,8 +45,6 @@ document.addEventListener('click', function (event) {
 });
 
 
-
-
 const fixButtons = document.querySelectorAll('.donate__fix-button');
 const fieldInput = document.querySelector('.donate__field');
 
@@ -59,34 +57,156 @@ fixButtons.forEach(button => {
 
 
 
+
+
+
 // for Requisites
 
-const expandableContainer = document.querySelector('.expandable');
-const requisitesBlocks = document.querySelectorAll('.donate__requisites');
-const requisitesText = document.querySelector('.donate__requisite-text');
+const buttonsBlocks = document.querySelectorAll('.donate__buttons > div');
+const requisitesBlocks = document.querySelectorAll('.donate__requisites > div');
 
-function toggleExpand(event) {
-  const requisites = event.currentTarget.getAttribute('data-requisites');
+const modal = document.getElementById('modal__requisites-data');
+const modalTitle2 = document.getElementById('requisitesTitle');
 
-  if (expandableContainer.classList.contains('expanded')) {
-    expandableContainer.classList.remove('expanded');
-    requisitesText.textContent = '';
-  } else {
-    expandableContainer.classList.add('expanded');
-    requisitesText.textContent = requisites;
+const paypalWhite = './src/styles/images/paypal-logo-white.svg';
+const moneygramWhite = './src/styles/images/moneygram-logo-white.svg';
+const westernWhite = './src/styles/images/westernunion-logo-white.svg';
+
+const paypalText = document.querySelector('.donate__button-text-paypal');
+const moneygramText = document.querySelector('.donate__button-text-moneygram');
+const westernText = document.querySelector('.donate__button-text-western');
+
+const paypalImage = document.querySelector('.donate__paypal-link img');
+const moneygramImage = document.querySelector('.donate__moneygram-link img');
+const westernImage = document.querySelector('.donate__western-union-link img');
+
+let activeButton = null;
+
+function openModal(data, block) {
+  modal.classList.add('active');
+  document.querySelector('.donate-container').classList.add('active');
+
+  const modalTitle = document.getElementById('dynamicTitle');
+  const modalName = document.getElementById('dynamicName');
+  const modalText = document.getElementById('dynamicText');
+  const modalEmail = document.getElementById('dynamicEmail');
+  const modalNumber = document.getElementById('dynamicNumber');
+  const modalOrg = document.getElementById('dynamicOrg');
+  const modalEdrpu = document.getElementById('dynamicEdrpu');
+  const modalBank = document.getElementById('dynamicBank');
+  const modalIban = document.getElementById('dynamicIban');
+  const modalDisc = document.getElementById('dynamicDisc');
+
+  modalTitle.textContent = data.title;
+  modalName.textContent = data.name;
+  modalText.textContent = data.text;
+  modalEmail.textContent = data.email;
+  modalNumber.textContent = data.number;
+  modalTitle2.textContent = data.title2;
+  modalOrg.textContent = data.organization;
+  modalEdrpu.textContent = data.edrpu;
+  modalBank.textContent = data.bank;
+  modalIban.textContent = data.iban;
+  modalDisc.textContent = data.disclaimer;
+
+  if (activeButton) {
+    activeButton.classList.remove('active');
+    if (activeButton === block) {
+      activeButton = null;
+      closeModal();
+      return;
+    }
   }
+
+  // Возвращаем изображения к исходному состоянию перед активацией новой кнопки
+  paypalImage.src = './src/styles/images/paypal-logo.svg';
+  moneygramImage.src = './src/styles/images/moneygram-logo.svg';
+  westernImage.src = './src/styles/images/westernunion-logo.svg';
+
+  // Возвращаем цвет текста к исходному состоянию перед активацией новой кнопки
+  paypalText.style.color = '#202020';
+  moneygramText.style.color = '#202020';
+  westernText.style.color = '#202020';
+
+  // Обновляем изображения и цвет текста, если кнопка активна
+  if (block.classList.contains('donate__paypal')) {
+    paypalImage.src = paypalWhite;
+    paypalText.style.color = '#fff';
+  } else if (block.classList.contains('donate__moneygram')) {
+    moneygramImage.src = moneygramWhite;
+    moneygramText.style.color = '#fff';
+  } else if (block.classList.contains('donate__western-union')) {
+    westernImage.src = westernWhite;
+    westernText.style.color = '#fff';
+  }
+
+  block.classList.add('active');
+  activeButton = block;
 }
 
-requisitesBlocks.forEach(block => {
-  block.addEventListener('click', toggleExpand);
+function closeModal() {
+  modal.classList.remove('active');
+  document.querySelector('.donate-container').classList.remove('active');
+  if (activeButton) {
+    activeButton.classList.remove('active');
+    activeButton = null;
+  }
+
+  // Возвращаем изображения к исходному состоянию
+  paypalImage.src = './src/styles/images/paypal-logo.svg';
+  moneygramImage.src = './src/styles/images/moneygram-logo.svg';
+  westernImage.src = './src/styles/images/westernunion-logo.svg';
+  
+  // Возвращаем цвет текста к исходному состоянию
+  paypalText.style.color = '#202020';
+  moneygramText.style.color = '#202020';
+  westernText.style.color = '#202020';
+}
+
+buttonsBlocks.forEach(block => {
+  block.addEventListener('click', function(event) {
+    const data = {
+      title: block.getAttribute('data-title'),
+      name: block.getAttribute('data-name'),
+      text: block.getAttribute('data-text'),
+      email: block.getAttribute('data-email'),
+      number: block.getAttribute('data-number'),
+      title2: '',
+      organization: '',
+      edrpu: '',
+      bank: '',
+      iban: '',
+      disclaimer: ''
+    };
+
+    openModal(data, block);
+  });
 });
 
-expandableContainer.addEventListener('click', (event) => {
-  if (event.target === expandableContainer) {
-    expandableContainer.classList.remove('expanded');
-    requisitesText.textContent = '';
+requisitesBlocks.forEach(block => {
+  block.addEventListener('click', function(event) {
+    const data = {
+      title: '',
+      name: '',
+      text: '',
+      email: '',
+      number: '',
+      title2: block.getAttribute('data-title2'),
+      organization: block.getAttribute('data-organization'),
+      edrpu: block.getAttribute('data-edrpu'),
+      bank: block.getAttribute('data-bank'),
+      iban: block.getAttribute('data-iban'),
+      disclaimer: block.getAttribute('data-disclaimer')
+    };
+
+    openModal(data, block);
+  });
+});
+
+document.addEventListener('click', function(event) {
+  const target = event.target;
+
+  if (!target.closest('.requisites-modal') && !target.closest('.donate__requisites') && !target.closest('.donate__buttons')) {
+    closeModal();
   }
 });
-
-
-
